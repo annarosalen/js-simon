f14e0f44b15ef524297b3c4d921d45fac2135b9d
Anna <rosalenanna@gmail.com>
2020-11-05 15:16:10 +0100

    logica iniziale

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+// Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
+
+// genero 5 numeri random e li salvo
+
+// inserisco i numeri in un alert
+
+// passano 30 secondi
+
+// l'utente inserisce in 5 prompt i numeri
+
+// se numeri diversi da alert: hai perso + quanti + quali numeri ha indovinato
+
+// se numeri uguali a alert : hai vinto
