1128cf53231e9148d4850a821ac787628ea1201c
Anna <rosalenanna@gmail.com>
2020-11-06 18:11:50 +0100

    css start e generazione gioco

--- js/myscript.js
+++ js/myscript.js
@@ -17,63 +17,63 @@ $(document).ready(function(){
       arrayRandom.push(numRandom);
     }
   }
+  console.log(arrayRandom);
 
   $("#random").text(arrayRandom);
 
-  // clicco start scompare bottone e compare arrayrandom
+  // clicco start (da qui si genera tutto)
   $(".start").click(function(){
+    // scompare bottone
     $(".start").hide();
-    $("#random").removeClass("hide");
-    $("#istruzioni").removeClass("hide");
-  });
+    // compare arrayrandom e istruzioni gioco
+    $("#random").removeClass("nascondi");
+    $("#istruzioni").removeClass("nascondi");
+    // dopo tot secondi scompaiono i numeri random e inizia conto alla rovescia
+    setTimeout(function(){
+      $("#random").addClass("nascondi");
+      $("#istruzioni").addClass("nascondi");
+    },5000)
 
+    // passano 30 secondi (prova 7 secondi)
+    setTimeout(inserisciNum,7000);
 
-  // creo funzione per far scomparire i numeri random dopo 5 secondi (3 secondi prova)
-  setTimeout(function(){
-    $("#random").addClass("hide");
-    $("#istruzioni").addClass("hide");
-  },3000)
+    // l'utente inserisce in 5 prompt i numeri e li salvo
+    var arrayUtente = [];
 
-  // passano 30 secondi (prova 5 secondi)
-  var tempo = setTimeout(inserisciNum,5000);
+    function inserisciNum(){
 
+      var i = 0;
+      while (i < 5) {
 
-  // l'utente inserisce in 5 prompt i numeri e li salvo
-  var arrayUtente = [];
+        var numUtente = arrayUtente[i];
+        numUtente = parseInt(prompt("INSERISCI I NUMERI CHE TI RICORDI UNO ALLA VOLTA"));
 
-  function inserisciNum(){
+        // se numero inserito da utente è uguale a num dell arrayUtente non contarlo (ripeti richiesta) + alert
+        if(arrayUtente.includes(numUtente)){
+          alert("HAI GIA' INSERITO QUESTO NUMERO");
+          i--;
 
+          // altrimenti se diverso a numRandom dell'arrayRandom: hai perso + quanti + quali numeri ha indovinato
+        }else if(!arrayRandom.includes(numUtente)){
+          console.log("Hai perso! " + "Hai indovinato un totale di " + arrayUtente.length + " numero/i," + "i numeri che hai indovinato sono: " + arrayUtente);
+          return;
 
-    var i = 0;
-    while (i < 5) {
+          // altrimenti se numero inserito è uguale a numRandom dell'arrayRandom pushalo nell' arrayUtente
+        }else{
+          arrayUtente.push(numUtente);
+        }
+        // e ripeti ciclo
+        i++;
+      } //fine ciclo while
 
-      var numUtente = arrayUtente[i];
-      numUtente = parseInt(prompt("INSERISCI I NUMERI CHE TI RICORDI UNO ALLA VOLTA"));
-
-      // se numero inserito da utente è uguale a num dell arrayUtente non contarlo (ripeti richiesta) + alert
-      if(arrayUtente.includes(numUtente)){
-        alert("HAI GIA' INSERITO QUESTO NUMERO");
-        i--;
-
-        // altrimenti se diverso a numRandom dell'arrayRandom: hai perso + quanti + quali numeri ha indovinato
-      }else if(!arrayRandom.includes(numUtente)){
-        console.log("Hai perso! " + "Hai indovinato un totale di " + arrayUtente.length + " numero/i," + "i numeri che hai indovinato sono: " + arrayUtente);
-        return;
-
-        // altrimenti se numero inserito è uguale a numRandom dell'arrayRandom pushalo nell' arrayUtente
-      }else{
-        arrayUtente.push(numUtente);
+      // se l'inserimento dell'utente va a buon fine per 5 volte: hai vinto
+      if(arrayUtente.length === 5){
+        console.log("hai vinto! HAI INDOVINATO TUTTI I NUMERI");
       }
-      // e ripeti ciclo
-      i++;
-    } //fine ciclo while
 
-    // se l'inserimento dell'utente va a buon fine per 5 volte: hai vinto
-    if(arrayUtente.length === 5){
-      console.log("hai vinto! HAI INDOVINATO TUTTI I NUMERI");
-    }
+    }//fine funzione inserisciNum
 
-  }//fine funzione inserisciNum
+  }); //fine start click
 
 
 }); //fine document ready
